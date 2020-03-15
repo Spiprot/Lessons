b8e44d3ad2aded752139194b953ccc4b58bc129b
Spiprot <49907114+Spiprot@users.noreply.github.com>
2020-03-15 23:20:45 +0500

    Выполнена задача 2

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.sandbox;
+
+public class Point {
+
+    double x;
+    double y;
+
+    Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    double distance(Point p) {
+        return Math.sqrt(Math.pow((this.x - p.x), 2.0) + Math.pow((this.y - p.y), 2.0));
+    }
+}
